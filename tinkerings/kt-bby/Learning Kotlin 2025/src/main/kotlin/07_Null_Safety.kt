22685957e056dc86014b906d85ccdfd0ce26f9ea
Joseph G <josef.emu@gmail.com>
2025-11-10 13:19:17 +0300

    changes updated to main repo

--- tinkerings/kt-bby/Learning Kotlin 2025/src/main/kotlin/07_Null_Safety.kt
+++ tinkerings/kt-bby/Learning Kotlin 2025/src/main/kotlin/07_Null_Safety.kt
@@ -3,12 +3,18 @@ fun main(){
     val tobesReview = CoffeeReview("Tobes", "Was okay", 3)
     val hueyReview = CoffeeReview("Riley", "Sijapenda", 0)
 
-    val somn: Int = null
+    // val somn: Int = null // results in an error as Int is a non-null type, resolved by adding a ?
+    val somn: Int? = 1
+
     println(somn)
+
 
     println("${hueyReview.name}, ${hueyReview.comment}, ${hueyReview.stars}")
     println("${tobesReview.name}, ${tobesReview.comment}, ${tobesReview.stars}")
     println("${saraReview.name}, ${saraReview.comment}, ${saraReview.stars}")
+
+    val saraLee: Int? = 5 //  results in an error> Argument type mismatch: actual type is 'Int?', but 'Int' was expected.
+    printReview("Ngoso", saraLee)
 
 }
 
@@ -18,3 +24,6 @@ class CoffeeReview (
     val comment: String,
     val stars: Int
 )
+
+fun printReview(sname: String,sstars: Int) =
+    println("$sname gave it $sstars stars!")
